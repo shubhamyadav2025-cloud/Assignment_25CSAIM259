9521f47859a80282a179194433fecbffb9c8dbbd
Shubham Yadav <shubham.yadav2025@glbajajgroup,org>
2026-06-13 10:51:46 +0530

    Day-5 assignment

---
+++ Day-5/Find_largest_prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+ {
+    int n, largest = -1;
+
+    printf("Enter a number");
+    scanf("%d", &n);
+
+    for(int i = 2; i <= n; i++)
+     {
+        while(n % i == 0)
+         {
+            largest = i;
+            n /= i;
+        }
+    }
+
+    printf("Largest Prime Factor = %d", largest);
+
+    return 0;
+}
