f6e5499e22d32ab01bf76ad83a7bf88c0383c6e6
Shubham Yadav <shubham.yadav2025@glbajajgroup,org>
2026-07-05 12:04:17 +0530

    Day-27 assignment

---
+++ Day-27/Create_marksheet_generstion_system.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[50];
+    int m1, m2, m3, m4, m5;
+    int total;
+    float percentage;
+
+    printf("Enter Student Name: ");
+    scanf("%s", name);
+
+    printf("Enter Marks of 5 Subjects:\n");
+    scanf("%d%d%d%d%d", &m1, &m2, &m3, &m4, &m5);
+
+    total = m1 + m2 + m3 + m4 + m5;
+    percentage = total / 5.0;
+
+    printf("\n--- Marksheet ---\n");
+    printf("Name        : %s\n", name);
+    printf("Total Marks : %d\n", total);
+    printf("Percentage  : %.2f%%\n", percentage);
+
+    if (percentage >= 90)
+        printf("Grade : A+\n");
+    else if (percentage >= 75)
+        printf("Grade : A\n");
+    else if (percentage >= 60)
+        printf("Grade : B\n");
+    else if (percentage >= 50)
+        printf("Grade : C\n");
+    else
+        printf("Grade : Fail\n");
+
+    return 0;
+}
